1d3eaba7398e3150f7c84abdb55b173dd0a4bbb9
Travis Decker <decker4693@gmail.com>
2019-01-13 19:44:11 -0700

    Initial commit, makeNegative.

---
+++ test/straylense/space/kataTest.java
@@ -0,0 +1,12 @@
+package straylense.space;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class kataTest {
+
+  @Test
+  void makeNegative() {
+    assertEquals(-42, kata.makeNegative(42) );
+  }
+}
